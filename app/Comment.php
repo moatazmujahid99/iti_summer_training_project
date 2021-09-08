0340987989091a08bfa44058b8b1585b7bf69723
moatazmujahid99 <forprofmoataz500@gmail.com>
2021-09-08 10:01:36 +0200

    creating project and database

---
+++ app/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'comment_text', 'user_id', 'post_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+}

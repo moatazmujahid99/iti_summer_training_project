2477eaaf331b1b0a009c5d731b5baf2eaa19515c
moatazmujahid99 <forprofmoataz500@gmail.com>
2021-09-11 00:53:20 +0200

    setup a user model

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Multicaret\Acquaintances\Traits\Friendable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Friendable;
 
     public function posts()
     {

3352ebd223842c5c50242cbc4471ed169eea8237
moatazmujahid99 <forprofmoataz500@gmail.com>
2021-09-12 00:18:13 +0200

    make user_img and post_img nullable and add them in fillable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('city')->nullable();
-            $table->string('user_img');
+            $table->string('user_img')->nullable();
             $table->text('description')->nullable();
             $table->string('birthdate')->nullable();
             $table->rememberToken();
--- database/migrations/2021_09_08_064333_create_posts_table.php
+++ database/migrations/2021_09_08_064333_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('description');
-            $table->string('post_img');
+            $table->string('post_img')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 

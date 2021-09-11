e759ced3e1c9f6e1eed024a39d7963b6f7aa0058
moatazmujahid99 <forprofmoataz500@gmail.com>
2021-09-11 05:12:49 +0200

    adding user_img and post_img fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('city')->nullable();
+            $table->string('user_img');
             $table->text('description')->nullable();
             $table->string('birthdate')->nullable();
             $table->rememberToken();
--- database/migrations/2021_09_08_064333_create_posts_table.php
+++ database/migrations/2021_09_08_064333_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('description');
+            $table->string('post_img');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
